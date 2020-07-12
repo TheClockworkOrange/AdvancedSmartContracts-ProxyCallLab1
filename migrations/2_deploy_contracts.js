f5f4747f50bf3e6f07542f08a5ce139859d689ca
Chamber36 <59872875+Mwenge1988@users.noreply.github.com>
2020-07-11 20:23:33 -0400

    Lab 1: Welcome to the Nether Realm.

---
+++ migrations/2_deploy_contracts.js
@@ -0,0 +1,12 @@
+const ProxyDelegate = artifacts.require('ProxyDelegate');
+const ProxyCall = artifacts.require('ProxyCall');
+const SomeLibrary = artifacts.require('SomeLibrary');
+
+module.exports = function(deployer) {
+    deployer.deploy(SomeLibrary)
+    .then(async() => {
+        await deployer.deploy(ProxyDelegate, SomeLibrary.address);
+        await deployer.deploy(ProxyCall, SomeLibrary.address);
+    });
+};
+
